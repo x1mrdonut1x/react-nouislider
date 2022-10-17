4a390e91478f0819565c929f5dfc073f7f86cd68
Alex Niznik <alex.niznik@santanderconsumer.dk>
2022-10-17 16:52:56 +0200

    chore: update rollup config for published package

--- rollup.config.js
+++ rollup.config.js
@@ -1,59 +1,59 @@
-const path = require('path');
+const path = require("path");
 
-const babel = require('rollup-plugin-babel');
-const commonjs = require('rollup-plugin-commonjs');
-const nodeResolve = require('rollup-plugin-node-resolve');
-const { terser } = require('rollup-plugin-terser');
-const replace = require('rollup-plugin-replace');
+const babel = require("rollup-plugin-babel");
+const commonjs = require("rollup-plugin-commonjs");
+const nodeResolve = require("rollup-plugin-node-resolve");
+const { terser } = require("rollup-plugin-terser");
+const replace = require("rollup-plugin-replace");
 
-const { name, peerDependencies } = require('./package.json');
+const { peerDependencies } = require("./package.json");
 
-const NODE_ENV = process.env.NODE_ENV || 'production';
+const NODE_ENV = process.env.NODE_ENV || "production";
 
-const formats = ['cjs', 'esm', 'umd'];
+const formats = ["cjs", "esm", "umd"];
 
-const outputs = formats.map(format => ({
+const outputs = formats.map((format) => ({
   file: [
-    path.join(__dirname, 'dist', name),
+    path.join(__dirname, "dist", "nouislider-react"),
     format,
-    NODE_ENV === 'production' ? 'production.min' : false,
-    'js',
+    NODE_ENV === "production" ? "production.min" : false,
+    "js",
   ]
     .filter(Boolean)
-    .join('.'),
+    .join("."),
   format,
-  sourcemap: NODE_ENV === 'production',
+  sourcemap: NODE_ENV === "production",
   globals: {
-    react: 'React',
-    nouislider: 'nouislider'
+    react: "React",
+    nouislider: "nouislider",
   },
-  ...(format === 'umd' && { name: 'ReactNouislider' }),
+  ...(format === "umd" && { name: "ReactNouislider" }),
 }));
 
 const commonConfig = {
-  input: path.resolve(__dirname, 'src/index.js'),
+  input: path.resolve(__dirname, "src/index.js"),
   plugins: [
-    NODE_ENV === 'production' &&
+    NODE_ENV === "production" &&
       replace({
-        'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
+        "process.env.NODE_ENV": JSON.stringify(NODE_ENV),
       }),
 
     babel({
-      exclude: 'node_modules/**',
+      exclude: "node_modules/**",
     }),
 
     commonjs({
-      include: 'node_modules/**',
+      include: "node_modules/**",
     }),
 
     nodeResolve({
-      mainFields: ['jsnext:main', 'module'],
+      mainFields: ["jsnext:main", "module"],
       browser: true,
     }),
 
-    NODE_ENV === 'production' && terser(),
+    NODE_ENV === "production" && terser(),
   ].filter(Boolean),
   external: Object.keys(peerDependencies),
 };
 
-module.exports = outputs.map(output => ({ ...commonConfig, output }));
+module.exports = outputs.map((output) => ({ ...commonConfig, output }));
